7b6e232fd50311f147546be2389c28b2ec5bae08
Sarthak Singh <114976722+SarthakSinghIndian@users.noreply.github.com>
2022-10-05 08:26:40 +0530

    Add files via upload

---
+++ phone.py
@@ -0,0 +1,39 @@
+import sys
+from time import sleep
+import phonenumbers
+from phonenumbers import geocoder, carrier,timezone
+
+a = "\033[1;31;1mFind The Location With Phonenumbers \n \n"
+print(a.center(60))
+
+print("\033[1;30;1m___________________________\n ")
+t = "\033[1;33;1mEnter your number with country code.\n\n\033[1;36;1mEg... For India Format is like this : -\n\tnumber = +91your number\n"
+
+for letter in t:
+    sleep(0.08)
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    
+print("\033[1;30;1m___________________________\n")
+#writing finished
+while True:
+    try :
+        number = input("\033[1;32;1mEnter your number : -   ")
+        phone = phonenumbers.parse(number)
+        break
+    except Exception as e:
+        print(e)
+      
+country = geocoder.description_for_number(phone, "en")
+SIMcard = carrier.name_for_number(phone, "en")
+timeZone = timezone.time_zones_for_number(phone)
+print("\n\033[1;34;1mThe Country name is : - " , country)
+print("\033[1;35;1mThe SIM card is : -  " , SIMcard)
+print("\033[1;39;1mThe  TimeZone is  : - " , timeZone)
+
+t = "\n\033[1;33;1mThanks for using this program ^_^. \n " 
+
+for s in t:
+    sleep(0.08)
+    sys.stdout.write(s)
+    sys.stdout.flush()
